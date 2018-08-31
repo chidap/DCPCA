92ef1c03bb8a00e9d0f8162b4ae7393618c22f64
Chidananda Patra <chids_p@MacBook-Pro.local>
2018-08-31 21:36:21 +0530

    banner completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { MDBBootstrapModulesPro } from 'ng-uikit-pro-standard';
+import { MDBSpinningPreloader } from 'ng-uikit-pro-standard';
+
 
 import { AppComponent } from './app.component';
-
+import { AgmCoreModule } from '@agm/core';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { BannerComponent } from './home/banner/banner.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    BannerComponent
   ],
   imports: [
-    BrowserModule
+    AppRoutingModule,
+    BrowserModule,
+    MDBBootstrapModulesPro.forRoot(),
+    AgmCoreModule.forRoot({ apiKey: 'Your_api_key' }),
   ],
-  providers: [],
+  providers: [
+    MDBSpinningPreloader,
+  ],
+  schemas: [
+    NO_ERRORS_SCHEMA
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
